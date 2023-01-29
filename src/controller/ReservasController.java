6502977dfffa3fb969f9b88869f53bef7a355497
Alejandro Barrera <alejandro91dark@hotmail.es>
2023-01-29 02:10:02 -0300

    CRUD completo

--- src/controller/ReservasController.java
+++ src/controller/ReservasController.java
@@ -1,13 +1,14 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.util.List;
 
 import dao.ReservaDAO;
 import factory.ConnectionFactory;
 import model.Reserva;
 
 public class ReservasController {
-
 	private ReservaDAO reservaDAO;
 
 	public ReservasController() {
@@ -17,6 +18,22 @@ public class ReservasController {
 
 	public void guardar(Reserva reserva) {
 		this.reservaDAO.guardar(reserva);
+	}
+
+	public List<Reserva> buscar() {
+		return this.reservaDAO.buscar();
+	}
+
+	public List<Reserva> buscarId(String id) {
+		return this.reservaDAO.buscarId(id);
+	}
+
+	public void actualizar(Date fechaEntrada, Date fechaSalida, String valor, String formaPago, Integer id) {
+		this.reservaDAO.Actualizar(fechaEntrada, fechaSalida, valor, formaPago, id);
+	}
+
+	public void Eliminar(Integer id) {
+		this.reservaDAO.Eliminar(id);
 	}
 
 }
