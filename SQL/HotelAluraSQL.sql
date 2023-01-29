cb560e181afb46e984268d56d732df16e899857a
Alejandro Barrera <alejandro91dark@hotmail.es>
2023-01-29 00:19:33 -0300

    Se creo conexion con la base de datos mediante pool

---
+++ SQL/HotelAluraSQL.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE hotelalura;
+
+USE hotelalura;
+
+CREATE TABLE reservas(
+	id INT NOT NULL AUTO_INCREMENT,
+	fecha_entrada DATE NULL,
+	fecha_salida DATE NULL,
+	valor VARCHAR(45) NULL,
+	forma_pago VARCHAR(45) NULL,
+	PRIMARY KEY(id)
+)Engine=InnoDB;
+
+CREATE TABLE huespedes(
+	id INT NOT NULL AUTO_INCREMENT,
+	nombre VARCHAR(45) NULL,
+	apellido VARCHAR(45) NULL,
+	fecha_nacimiento DATE NULL,
+	nacionalidad VARCHAR(45) NULL,
+	telefono INT,
+	id_reserva INT NOT NULL,
+	PRIMARY KEY(id)
+)Engine=InnoDB;
+
+ALTER TABLE huespedes ADD FOREIGN KEY (id_reserva) REFERENCES reservas(id);
